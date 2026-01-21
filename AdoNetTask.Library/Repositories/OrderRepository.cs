49e88c00271c11732b51681b26210193f70c4c0c
Serigzhan Naviy <serigzhan_naviy@epam.com>
2026-01-21 11:46:36 +0500

    feat: add order repository logic

--- AdoNetTask.Library/Repositories/OrderRepository.cs
+++ AdoNetTask.Library/Repositories/OrderRepository.cs
@@ -1,32 +1,116 @@
-﻿namespace AdoNetTask.Library;
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace AdoNetTask.Library;
 
 public class OrderRepository(string connectionString) : IOrderRepository
 {
-
     private readonly string _connectionString = connectionString;
 
     public void Add(Order order)
     {
-        throw new NotImplementedException();
+        const string sql = @"
+            INSERT INTO [Order] (Status, CreatedDate, UpdatedDate, ProductId)
+            VALUES (@Status, @CreatedDate, @UpdatedDate, @ProductId);
+            SELECT SCOPE_IDENTITY();";
+
+        using var connection = new SqlConnection(_connectionString);
+        using var command = new SqlCommand(sql, connection);
+
+        command.Parameters.AddWithValue("@Status", order.Status.ToString());
+        command.Parameters.AddWithValue("@CreatedDate", order.CreatedDate);
+        command.Parameters.AddWithValue("@UpdatedDate", order.UpdatedDate);
+        command.Parameters.AddWithValue("@ProductId", order.ProductId);
+
+        connection.Open();
+        var id = Convert.ToInt32(command.ExecuteScalar());
+        order.Id = id;
     }
 
     public void Update(Order order)
     {
-        throw new NotImplementedException();
+        const string sql = @"
+            UPDATE [Order]
+            SET Status = @Status,
+                CreatedDate = @CreatedDate,
+                UpdatedDate = @UpdatedDate,
+                ProductId = @ProductId
+            WHERE Id = @Id";
+
+        using var connection = new SqlConnection(_connectionString);
+        using var command = new SqlCommand(sql, connection);
+
+        command.Parameters.AddWithValue("@Id", order.Id);
+        command.Parameters.AddWithValue("@Status", order.Status.ToString());
+        command.Parameters.AddWithValue("@CreatedDate", order.CreatedDate);
+        command.Parameters.AddWithValue("@UpdatedDate", order.UpdatedDate);
+        command.Parameters.AddWithValue("@ProductId", order.ProductId);
+
+        connection.Open();
+        command.ExecuteNonQuery();
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
-    }
+        const string sql = "DELETE FROM [Order] WHERE Id = @Id";
 
-    public void DeleteOrdersBulk(int? month, int? year, OrderStatus? status, int? productId)
-    {
-        throw new NotImplementedException();
+        using var connection = new SqlConnection(_connectionString);
+        using var command = new SqlCommand(sql, connection);
+
+        command.Parameters.AddWithValue("@Id", id);
+
+        connection.Open();
+        command.ExecuteNonQuery();
     }
 
     public IEnumerable<Order> GetFilteredOrders(int? month, int? year, OrderStatus? status, int? productId)
     {
-        throw new NotImplementedException();
+        var orders = new List<Order>();
+
+        using var connection = new SqlConnection(_connectionString);
+        using var command = new SqlCommand("spOrders_Filter", connection);
+
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.AddWithValue("@Month", (object?)month ?? DBNull.Value);
+        command.Parameters.AddWithValue("@Year", (object?)year ?? DBNull.Value);
+        command.Parameters.AddWithValue("@Status", status.HasValue ? status.Value.ToString() : DBNull.Value);
+        command.Parameters.AddWithValue("@ProductId", (object?)productId ?? DBNull.Value);
+
+        connection.Open();
+        using var reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            orders.Add(MapToOrder(reader));
+        }
+
+        return orders;
+    }
+
+    public void DeleteOrdersBulk(int? month, int? year, OrderStatus? status, int? productId)
+    {
+        using var connection = new SqlConnection(_connectionString);
+        using var command = new SqlCommand("spOrders_Bulk_Delete", connection);
+
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.AddWithValue("@Month", (object?)month ?? DBNull.Value);
+        command.Parameters.AddWithValue("@Year", (object?)year ?? DBNull.Value);
+        command.Parameters.AddWithValue("@Status", status.HasValue ? status.Value.ToString() : DBNull.Value);
+        command.Parameters.AddWithValue("@ProductId", (object?)productId ?? DBNull.Value);
+
+        connection.Open();
+        command.ExecuteNonQuery();
+    }
+
+    private static Order MapToOrder(SqlDataReader reader)
+    {
+        return new Order
+        {
+            Id = (int)reader["Id"],
+            Status = Enum.Parse<OrderStatus>((string)reader["Status"]),
+            CreatedDate = (DateTime)reader["CreatedDate"],
+            UpdatedDate = (DateTime)reader["UpdatedDate"],
+            ProductId = (int)reader["ProductId"]
+        };
     }
 }
--- AdoNetTask.SqlServer/dbo/Tables/Order.sql
+++ AdoNetTask.SqlServer/dbo/Tables/Order.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[Order]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Status] NVARCHAR(20) NOT NULL, 
-    [CreatedDate] DATETIME NOT NULL, 
-    [UpdatedDate] DATETIME NOT NULL, 
-    [ProductId] INT NOT NULL, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    [Status] NVARCHAR(20) NOT NULL,
+    [CreatedDate] DATETIME NOT NULL,
+    [UpdatedDate] DATETIME NOT NULL,
+    [ProductId] INT NOT NULL,
     CONSTRAINT [FK_Order_To_Product_Table] FOREIGN KEY ([ProductId]) REFERENCES [Product]([Id])
 )
